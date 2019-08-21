49f6cb17781388576cabe9d198312747e0a7ac14
kyle <kylekennedyw@gmail.com>
2019-08-20 19:51:05 -0500

    sorted set working

--- rl-demo-script.py
+++ rl-demo-script.py
@@ -15,17 +15,12 @@ def main():
         port=port1
     )
 
-    # define number of keys and array to hold them
-    num_keys = 100
-    keys = []
-
     print("starting inserts to redis-server-1")
 
-    # insert #s 1-100 into os db
+    # define number of keys and add them to sorted set on os db
+    num_keys = 100
     for i in range(1, num_keys+1):
-        key = "key-" + str(i)
-        keys.append(key)
-        r1.set(key, i)
+        r1.zadd("my-sset", {i: i})
 
     print('inserts complete. starting reads from redis-server-2')
 
@@ -36,10 +31,10 @@ def main():
     )
 
     # read vaules 100-1 from es db
-    for i in range(num_keys, 0, -1):
-        print(str(r2.get(keys[i-1]), 'utf-8'))
+    print(r2.zrevrange("my-sset", 0, -1))
 
-    print('reads complete. exiting')
+    print('reads complete. deleting key and exiting')
+    r1.delete("my-sset")
 
 if __name__ == '__main__':
     main()
